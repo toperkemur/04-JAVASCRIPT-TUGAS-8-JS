9e15df6336db6da39bcf80c00046806e8365efa0
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-04-18 20:00:52 +0700

    Initial Commit
    Menambahkan tugas8.js

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function tesObject(){
+var orang = {
+  nama : "Christoper",
+  umur : 31,
+  domisili: "Jakarta",
+  jeniskelamin: "laki-laki"
+}
+console.log(orang);
+orang.domisili = "Yogyakarta";
+console.log(orang);
+console.log(orang.domisili);
+
+}
+
+tesObject();
